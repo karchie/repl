3a72f252d2ee03af6073a72c5aaeccb3b303f3bc
Kevin A. Archie <karchie@wustl.edu>
2011-08-17 16:47:28 -0500

    added log4jr testing code

---
+++ src/repl/log4jr.clj
@@ -0,0 +1,21 @@
+(ns #^{:doc "Test log4jr server"
+       :author "Kevin A. Archie <karchie@wustl.edu>"}
+  nrg.log4jr
+  (:import org.nrg.log4j.PostAppender
+	   org.apache.log4j.spi.LoggingEvent
+	   org.apache.log4j.Logger
+	   org.apache.log4j.Level))
+
+(defn test-server [url & {:keys [message logger throwable]
+			  :or {message "Hi there"
+			       logger "org.nrg.clj.repl"
+			       throwable nil}}]
+  "Tests a remote log4jr server by sending it an event."
+  (let [logev (LoggingEvent. logger
+			     (Logger/getLogger logger)
+			     Level/INFO
+			     message
+			     throwable)]
+    (doto (PostAppender.)
+      (.setURL url)
+      (.append logev))))
